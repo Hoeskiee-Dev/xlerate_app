6310e82211c29c154a5633b0a32a1d3597bbfd7b
hoeskiee <hoeskiee@gmail.com>
2026-08-06 16:39:59 +0700

    Ref: Fix nullable check on user

--- lib/presentation/providers/programs/attended_program_provider.dart
+++ lib/presentation/providers/programs/attended_program_provider.dart
@@ -1,11 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:xlerate/domain/entities/program.dart';
 import 'package:xlerate/presentation/providers/programs/programs_list_provider.dart';
-import 'package:xlerate/presentation/providers/user_provider.dart'; // tempat menyimpan userId login
+import 'package:xlerate/presentation/providers/user_provider.dart';
 
 final attendedProgramProvider = Provider<AsyncValue<List<Program>>>((ref) {
-  final userAsync = ref.watch(userProvider);
-  final currentUserId = userAsync!.id;
+  final user = ref.watch(userProvider);
+
+  if (user == null) {
+    return const AsyncData([]);
+  }
+
+  final currentUserId = user.id;
 
   final allProgramsAsync = ref.watch(programsListProvider);
 
